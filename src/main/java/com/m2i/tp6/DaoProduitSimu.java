36b07ec5e57a8ac9eab80adbd0434db5a79ede35
Didier Defrance <d2f.defrance@gmail.com>
2018-12-12 16:49:05 +0100

    debut dao simu

--- src/main/java/com/m2i/tp6/DaoProduitSimu.java
+++ src/main/java/com/m2i/tp6/DaoProduitSimu.java
@@ -1,17 +1,32 @@
 package com.m2i.tp6;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * Classe de simulation en mémoire sans base de données
+ */
 public class DaoProduitSimu implements DaoProduit {
+	private Long numeroMax = null; //pour simuler auto_increment
+	private Map<Long,Produit> mapProduits = new HashMap<Long,Produit>();
+	
+	public DaoProduitSimu() {
+		mapProduits.put(1L, new Produit(1L,"cahier",2.5));
+		mapProduits.put(2L, new Produit(2L,"stylo",1.5));
+		mapProduits.put(3L, new Produit(3L,"trousse",3.5));
+		mapProduits.put(4L, new Produit(4L,"classeur",7.5));
+		numeroMax=4L;
+	}
+	//avant appel le numero du produit est null (inconnu)
+	//apres appel , ce numero sera connu (auto incrémenté)
+	public void ajouterProduit(Produit p) {
+		p.setNumero(++numeroMax);//simuler auto_increment
+		mapProduits.put(p.getNumero(), p);
+	}
 
 	public List<Produit> rechercherProduits() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Produit>( mapProduits.values() );
 	}
-
-	public void ajouterProduit(Produit p) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
